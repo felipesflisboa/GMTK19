c8e007aa997bbc1d083e6ee74e4dda48aa1136ef
felipesflisboa <felipesflisboa@users.noreply.github.com>
2019-08-25 13:56:22 -0300

    fix: High Score screen on differents resolutions

--- Assets/Scripts/Reusable/ScoreList/ScoreListDrawer.cs
+++ Assets/Scripts/Reusable/ScoreList/ScoreListDrawer.cs
@@ -3,23 +3,31 @@ using UnityEngine.UI;
 
 /// <summary>
 /// Draw a Score List. Put on an empty object as pivot.
-/// Version 4.2X
+/// Version 4.4
 /// </summary>
 public abstract class ScoreListDrawer<TScoreList, TScoreListGeneric> : MonoBehaviour
-    where TScoreListGeneric : struct, System.IComparable
-    where TScoreList : ScoreList<TScoreListGeneric>, new() {
-    public GameObject textScorePrefab; // Y start and Y gain are pick on prefab Y position and height values 
-    public float yGainValue;
-    public float yGainRatio; // Based on total screen size
-    public Text labelCurrentScore; // Current Score label to be show if exist
-    public Text textCurrentScore; // Current Score text value to be show if exist
-    public string newRecordFormatMessage = "{0} NEW RECORD!";
-    public bool clearLastScoreAfterShowing;
+		where TScoreListGeneric : struct, System.IComparable
+		where TScoreList : ScoreList<TScoreListGeneric>, new() {
+    public GameObject textScorePrefab;
+	public Transform textScoreParent; // Parent to instantiate textScorePrefab. If null uses self
+	public string newRecordFormatMessage = "{0} !";
+	public bool clearLastScoreAfterShowing;
+	bool showedLastScore;
 
-    // Override this property with a static variable
-    protected abstract TScoreListGeneric? LastScore { get; set; }
+	[Header("Current Score")]
+	public Text currentScoreLabel; // Current Score label to be show
+    public Text currentScoreText; // Current Score text value to be show
+	public string currentScoreDefaultMessage; // When empty, simply disable 
 
-    protected virtual void Start() {
+	// Override this property with a static variable
+	protected abstract TScoreListGeneric? LastScore { get; set; }
+	
+	protected virtual void Awake() {
+		if (!textScoreParent)
+			textScoreParent = transform;
+	}
+
+	protected virtual void Start() {
         Draw();
     }
 
@@ -32,40 +40,45 @@ public abstract class ScoreListDrawer<TScoreList, TScoreListGeneric> : MonoBehav
     /// Draw the prefabs.
     /// </summary>
     public virtual void Draw() {
-        // The Y start is the prefab Y position
-        bool showedLastScore = false;
+		showedLastScore = false;
         TScoreList scoreList = new TScoreList();
         scoreList.Load();
-        for (int i = 0; i < scoreList.Size; i++) {
-            Text textScore = Instantiate(textScorePrefab).GetComponent<Text>();
-            textScore.transform.SetParent(transform, false);
-            textScore.transform.localPosition = CalculateLocalPosition(i);
-            textScore.text = string.Format("{0:00} {1}", i + 1, scoreList.GetString(i));
-            // NEW RECORD Label		
-            if (LastScore != null && !showedLastScore) {
-                // Cast values to compare with Mathf.Approximately;
-                float value = (float)System.Convert.ChangeType(scoreList.values[i], typeof(float));
-                float lastScore = (float)System.Convert.ChangeType(LastScore, typeof(float));
-                if (Mathf.Approximately(value, lastScore)) {
-
-                    FormatTextNewRecord(textScore);
-                    showedLastScore = true;
-                }
-            }
-        }
-        if (labelCurrentScore != null && LastScore == null) {
-            labelCurrentScore.gameObject.SetActive(false);
-        }
-        if (textCurrentScore != null) {
-            if (LastScore == null) {
-                textCurrentScore.gameObject.SetActive(false);
-            } else {
-                textCurrentScore.text = scoreList.GetStringAsValue((TScoreListGeneric)LastScore);
-            }
-        }
-        if (clearLastScoreAfterShowing)
+        for (int i = 0; i < scoreList.Size; i++)
+			CreateTextScore(scoreList, i);
+		RefreshCurrentScore(scoreList);
+		if (clearLastScoreAfterShowing)
             LastScore = null;
-    }
+	}
+
+	protected virtual void RefreshCurrentScore(TScoreList scoreList) {
+		if (currentScoreLabel != null)
+			currentScoreLabel.gameObject.SetActive(LastScore != null || !string.IsNullOrEmpty(currentScoreDefaultMessage));
+		if (currentScoreText != null) {
+			if (LastScore == null && string.IsNullOrEmpty(currentScoreDefaultMessage)) {
+				currentScoreText.gameObject.SetActive(false);
+			} else {
+				currentScoreText.gameObject.SetActive(true);
+				currentScoreText.text = LastScore == null ? currentScoreDefaultMessage : scoreList.GetStringAsValue((TScoreListGeneric)LastScore);
+			}
+		}
+	}
+
+	protected Text CreateTextScore(TScoreList scoreList, int index) {
+		Text ret = Instantiate(textScorePrefab, transform).GetComponent<Text>();
+		ret.text = string.Format("{0:00} {1}", index + 1, scoreList.GetString(index));
+		if (!showedLastScore && IsNewRecord(scoreList.values[index])) {
+			FormatTextNewRecord(ret);
+			showedLastScore = true;
+		}
+		return ret;
+	}
+
+	protected bool IsNewRecord(TScoreListGeneric score) {
+		return LastScore != null && Mathf.Approximately(
+			(float)System.Convert.ChangeType(score, typeof(float)),
+			(float)System.Convert.ChangeType(LastScore, typeof(float))
+		);
+	}
 
     /// <summary>
     /// Receive a text component and format it with New Record text.
@@ -75,18 +88,10 @@ public abstract class ScoreListDrawer<TScoreList, TScoreListGeneric> : MonoBehav
     }
 
     /// <summary>
-    /// Calculates the local position of each item.
-    /// </summary>
-    protected virtual Vector3 CalculateLocalPosition(int index) {
-        return Vector3.up * (yGainValue * index + Screen.height * index * yGainRatio);
-    }
-
-    /// <summary>
     /// Remove all children.
     /// </summary>
     public virtual void Clear() {
-        foreach (Transform child in transform) {
+		foreach (Transform child in textScoreParent)
             Destroy(child.gameObject);
-        }
     }
 }
